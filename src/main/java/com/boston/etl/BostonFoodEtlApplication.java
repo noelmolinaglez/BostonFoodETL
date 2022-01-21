688b459fc1e9821739ea04a30e0a72ba5d2f3823
Noel Molina Gonzalez <noelmolinaglez@gmail.com>
2022-01-20 22:14:16 -0500

    Initial Commit

---
+++ src/main/java/com/boston/etl/BostonFoodEtlApplication.java
@@ -0,0 +1,13 @@
+package com.boston.etl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BostonFoodEtlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BostonFoodEtlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boston/etl/BostonFoodEtlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boston.etl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BostonFoodEtlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
